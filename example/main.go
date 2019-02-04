064624ea7ac0e701040b781b6dbcb28305eb4aca
Simon Ordish <simon.ordish@masagi.co.uk>
2019-02-04 11:27:39 +0000

    Removed NHS

--- example/main.go
+++ example/main.go
@@ -18,31 +18,9 @@ var (
 	numberValue      int
 )
 
-func healthReport() gocore.NHSReport {
-	var dependencies [2]gocore.Dependency
-	dependencies[0].Service = "Service 1"
-	dependencies[0].Status = "AMBER"
-	dependencies[1].Service = "Service 2"
-	dependencies[1].Status = "GREEN"
-	return gocore.NHSReport{
-		Dependencies: dependencies[:],
-		Status:       "GREEN",
-	}
-}
-
 func init() {
 	stats := gocore.Config().Stats()
 	log.Printf("STATS\n%s\nVERSION\n-------\n%s (%s)\n\n", stats, version, commit)
-
-	nhsURL, _ := gocore.Config().Get("nhsUrl")
-
-	config := gocore.NHSConfig{
-		Name:     "Example Microservice Instance 1",
-		Type:     "Example MicroService",
-		URL:      nhsURL,
-		Callback: healthReport,
-	}
-	gocore.Register(config)
 
 	value, _ = gocore.Config().Get("key")
 	valueWithDefault, _ = gocore.Config().Get("anotherKey", "default")
