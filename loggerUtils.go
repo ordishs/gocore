aa4728a5239ee92ca22d544fbaf0f1876b90752d
Simon Ordish <simon.ordish@masagi.co.uk>
2019-08-14 16:05:52 +0200

    Add Newline to trace if necessary

--- loggerUtils.go
+++ loggerUtils.go
@@ -36,9 +36,16 @@ func (l *Logger) sendToTrace(format string, msg string, level string, args ...in
 	l.conf.mu.Lock()
 	defer l.conf.mu.Unlock()
 
+	fullMsg := fmt.Sprintf(format, args...)
+	buf := make([]byte, 0)
+	buf = append(buf, fullMsg...)
+	if len(fullMsg) == 0 || fullMsg[len(fullMsg)-1] != '\n' {
+		buf = append(buf, '\n')
+	}
+
 	for s, r := range l.conf.trace.sockets {
 		if l.isRegexMatch(r, fmt.Sprintf(msg, args...)) || l.isRegexMatch(strings.ToLower(r), strings.ToLower(level)) {
-			_, e := s.Write([]byte(fmt.Sprintf(format, args...)))
+			_, e := s.Write(buf)
 			if e != nil {
 				log.Println(ansi.Color(fmt.Sprintf("Writing client error: '%s'", e), "red"))
 				delete(l.conf.trace.sockets, s)
